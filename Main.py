6277b909d17495473889c3630bafb83211a0e7d0
daniel-aj-brown <112202582+daniel-aj-brown@users.noreply.github.com>
2022-08-27 21:56:24 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,28 @@
+import pygame as pg
+
+class Engine:
+    def __init__(self, width, height, fps):
+        pg.init()
+        self.RES = self.WIDTH, self.HEIGHT = width, height
+        self.FPS = fps
+        self.screen = pg.display.set_mode(self.RES)
+        self.clock = pg.time.Clock()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill(pg.Color("darkslategray"))
+
+    def run(self):
+        while True:
+            self.update()
+            self.draw()
+            [exit() for i in pg.event.get() if i.type == pg.QUIT]
+            pg.display.set_caption(str(self.clock.get_fps()))
+            pg.display.flip()
+            self.clock.tick(self.FPS)
+
+if __name__ == "__main__":
+    app = Engine(800, 600, 60)
+    app.run()
